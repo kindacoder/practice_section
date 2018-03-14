f079ae3deebd2cf25c2920b0fd61f9bbe3bdaee6
kindacoder <code.ashutosh@gmail.com>
2018-03-14 22:48:53 +0530

    Google map api practice

--- git practice/file01.js
+++ git practice/file01.js
@@ -2,6 +2,8 @@ function greet(name) {
     console.log('having git branching ' + name);
     console.log('having git branching ' + name);
     console.log('having git branching ' + name);
+    console.log('nice to have you here ');
+    console.log('Hello ashutosh');
 
 }
 greet('Ashutosh')
